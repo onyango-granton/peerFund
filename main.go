3c60650ebbb440e1f96867f66cdc84349aacdb6b
JosephOkumu <jokumu25@gmail.com>
2024-07-31 08:09:17 +0300

    Modifying input arguments and output

--- main.go
+++ main.go
@@ -8,166 +8,109 @@ import (
 	"log"
 	"math/big"
 	"net/http"
-	"time"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-var client *ethclient.Client
-
-type TransferRequest struct {
-	ToAddress string `json:"toAddress"`
-	Amount    string `json:"amount"`
+type Request struct {
+	Recipient  string `json:"recipient"`
+	Amount     string `json:"amount"`
+	PrivateKey string `json:"privateKey"`
 }
 
-type TransferResponse struct {
-	FromAddress string `json:"fromAddress"`
-	ToAddress   string `json:"toAddress"`
-	Amount      string `json:"amount"`
-	Balance     string `json:"balance"`
-	Message     string `json:"message"`
+type Response struct {
+	Message string `json:"message"`
+	Balance string `json:"balance"`
 }
 
 func main() {
-	var err error
-	client, err = connectToEthereum()
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-	}
-
-	http.HandleFunc("/transfer", transferHandler)
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-
-	fmt.Println("Server started at http://localhost/:8080")
+	http.HandleFunc("/sendEth", sendEthHandler)
+	log.Println("Server is listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func connectToEthereum() (*ethclient.Client, error) {
-	client, err := ethclient.Dial("HTTP://127.0.0.1:7545")
+func sendEthHandler(w http.ResponseWriter, r *http.Request) {
+	var req Request
+	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		return nil, err
-	}
-	fmt.Println("We have a connection")
-	return client, nil
-}
-
-func transferHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	var req TransferRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Error decoding request", http.StatusBadRequest)
-		return
-	}
-
-	toAddress := req.ToAddress
-	amount := req.Amount
-
-	response, err := transferETH(toAddress, amount)
+	client, err := ethclient.Dial("HTTP://127.0.0.1:7545")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+
+	privateKey, err := crypto.HexToECDSA(req.PrivateKey)
+	if err != nil {
+		http.Error(w, "Invalid private key", http.StatusBadRequest)
+		return
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		http.Error(w, "Cannot assert type: publicKey is not of type *ecdsa.PublicKey", http.StatusBadRequest)
+		return
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	value := new(big.Int)
+	value.SetString(req.Amount, 10)
+	value = value.Mul(value, big.NewInt(1e18)) // Convert to Wei
+
+	gasLimit := uint64(21000)
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	toAddress := common.HexToAddress(req.Recipient)
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID := big.NewInt(1337)
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	message := fmt.Sprintf("Transaction sent: %s", signedTx.Hash().Hex())
+	balance := getBalanceInEth(client, fromAddress)
+
+	response := Response{
+		Message: message,
+		Balance: balance,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
-func checkBalance(client *ethclient.Client, addressStr string) (*big.Int, error) {
-	address := common.HexToAddress(addressStr)
+func getBalanceInEth(client *ethclient.Client, address common.Address) string {
 	balance, err := client.BalanceAt(context.Background(), address, nil)
 	if err != nil {
-		return nil, err
+		return fmt.Sprintf("Error getting balance: %v", err)
 	}
-	return balance, nil
-}
-
-func transferETH(toAddress string, amount string) (*TransferResponse, error) {
-	privateKey, err := crypto.HexToECDSA("4237445a301a6e009ce66f468745aeca78dfaab83f6b451c93ad92586392d940")
-	if err != nil {
-		return nil, fmt.Errorf("error loading private key: %v", err)
-	}
-
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		return nil, fmt.Errorf("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
-	}
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		return nil, fmt.Errorf("error getting nonce: %v", err)
-	}
-
-	value := new(big.Int)
-	value, ok = value.SetString(amount, 10)
-	if !ok {
-		return nil, fmt.Errorf("invalid amount")
-	}
-	gasLimit := uint64(21000)
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		return nil, fmt.Errorf("error getting suggested gas price: %v", err)
-	}
-
-	toAddressHex := common.HexToAddress(toAddress)
-	var data []byte
-	tx := types.NewTransaction(nonce, toAddressHex, value, gasLimit, gasPrice, data)
-
-	chainID := big.NewInt(1337)
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
-	if err != nil {
-		return nil, fmt.Errorf("error signing transaction: %v", err)
-	}
-
-	err = client.SendTransaction(context.Background(), signedTx)
-	if err != nil {
-		return nil, fmt.Errorf("error sending transaction: %v", err)
-	}
-
-	receipt, err := waitForTx(client, signedTx.Hash())
-	if err != nil {
-		return nil, fmt.Errorf("error waiting for transaction: %v", err)
-	}
-
-	message := "Transaction failed"
-	if receipt.Status == types.ReceiptStatusSuccessful {
-		message = "Transaction successful"
-	}
-
-	balance, err := checkBalance(client, fromAddress.Hex())
-	if err != nil {
-		return nil, fmt.Errorf("error checking balance: %v", err)
-	}
-
-	response := &TransferResponse{
-		FromAddress: fromAddress.Hex(),
-		ToAddress:   toAddressHex.Hex(),
-		Amount:      value.String(),
-		Balance:     balance.String(),
-		Message:     message,
-	}
-
-	return response, nil
-}
-
-func waitForTx(client *ethclient.Client, txHash common.Hash) (*types.Receipt, error) {
-	for {
-		receipt, err := client.TransactionReceipt(context.Background(), txHash)
-		if err == nil {
-			return receipt, nil
-		}
-		if err != nil && err != ethereum.NotFound {
-			return nil, err
-		}
-		time.Sleep(time.Second)
-	}
+	ethValue := new(big.Float).Quo(new(big.Float).SetInt(balance), big.NewFloat(1e18))
+	return fmt.Sprintf("Balance: %s ETH", ethValue.Text('f', 18))
 }
